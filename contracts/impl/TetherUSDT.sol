2d553e5df4fb56b4f089964dcb8b1d2c387f01f4
Andrew Miracle <me@andrewmiracle.com>
2021-12-01 01:05:03 +0000

    add smart contract modules

--- contracts/impl/TetherUSDT.sol
+++ contracts/impl/TetherUSDT.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
-import { BEP20 } from "../overrides/BEP20Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 
 // USDT Faucet impl
-contract TetherUSDT is BEP20 {
-    constructor() payable BEP20("Tether USDT", "USDT", 9) {
+contract TetherUSDT is ERC20 {
+    constructor() payable ERC20("Tether USDT", "USDT") {
         _mint(msg.sender, 103550);
     }
 }
