7912423acc489f3a5b89a1cd83a9fcd2aec1b582
Andrew Miracle <me@andrewmiracle.com>
2021-12-20 02:26:22 +0000

    boostrap idea for homepae

--- dapp/src/views/public/Homepage/index.tsx
+++ dapp/src/views/public/Homepage/index.tsx
@@ -12,15 +12,25 @@ export default function Homepage() {
     <>
       {/* ----------------- Render React Children in this layout from here --------------- */}
       <section>
+        {/* ---- Menu items: Product, Features, Developer Docs, {Button: Enter DApp} */}
         <Hero />
-        {/* --- for events, for course completion, memberships and DAOS */}
+        {/* --- for events, for course completion, memberships and DAOS and institution identification, conferences, hackathons, */}
         <FeatureBox />
-        {/* --- Multiple chain --- Multiple Data sources to pull records and dynamic info::: use GIF */}
+        {/* --- Multiple chain support --- Multiple Data sources to pull records and dynamic info::: use GIF */}
         <MultichainFeature />
         <SimplePricing />
 
-        {/* ---- save the best for last ------ */}
+        {/* ---- save the best for last::: coming soon------ */}
+        {/* Features we're thinking about::::>> Developer API to integrate to your platform */}
+        {/* --- pay to redeem certifications, charge people to redeem their certificates and certifications */}
+        {/* --- Multi chain Bridge >>><<< Hybrid Smart contracts::: use hybrid contracts that use oracles and custody wallets for larger organizations with full on-chain visibility */}
+
+        {/* ----  */}
         <FeatureBox />
+
+        {/* Ready to issue to most valuable credential of your life*/}
+        {/* NFTs are full proof, of fraud, fake resumes and identification, with guarantee of the highest level of authenticity */}
+
         <CTACallout />
         {/* ---- save the best for last ------ */}
       </section>
