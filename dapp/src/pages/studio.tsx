a73ed676d6c4fba6533465f9705b77220b0fb236
Andrew Miracle <me@andrewmiracle.com>
2021-12-28 13:43:38 +0000

    add components for display and editor actions

--- dapp/src/pages/studio.tsx
+++ dapp/src/pages/studio.tsx
@@ -1,9 +1,6 @@
 import { GetServerSideProps } from 'next';
-
 import * as NextAuth from '@/lib/auth.helper';
-
 import Layout from '@/components/layout/PageLayout';
-
 import View from '@/views/app/Admin';
 
 export default function Default() {
