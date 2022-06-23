54d5a763e773b0d7e4383330de14bda59760f35f
"fps" <"anthony.nnaemeka.umeh@gmail.com">
2022-06-23 21:08:02 +0100

    ...

--- src/contracts/DaccredBase.sol
+++ src/contracts/DaccredBase.sol
@@ -67,11 +67,11 @@ contract DaccredBase {
     // Test
     // Token, TKN, 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
 
-    /// @dev    Emitted when user mints a token.
+    /// @dev Emitted when user mints a token.
     event BaseMint(address indexed from, address indexed to);
-    /// @dev    Emitted when `owner` enables `approved` to manage the `tokenId` token.
+    /// @dev Emitted when `owner` enables `approved` to manage the `tokenId` token.
     event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
-    /// @dev    Emitted when `owner` enables or disables (`approved`) `operator` to manage all of its assets.
+    /// @dev Emitted when `owner` enables or disables (`approved`) `operator` to manage all of its assets.
     event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
 
     /**
@@ -112,7 +112,7 @@ contract DaccredBase {
     * @dev  Returns the name of the Token.
     *       [Ref IERC721Metadata - name].
     *
-    * @return Token name.
+    * @return string Token name.
     */
     function name() public view returns(string memory) {
         /// @dev Returns the name of the token.
@@ -123,7 +123,7 @@ contract DaccredBase {
     * @dev  Returns the symbol of the Token.
     *       [Ref IERC721Metadata - symbol].
     *
-    * @return Token symbol.
+    * @return string Token symbol.
     */
     function symbol() public view returns(string memory) {
         /// @dev Returns the symbol of the token.
@@ -134,10 +134,11 @@ contract DaccredBase {
     * @dev  Returns the Uniform Resource Identifier (URI) for `tokenId` token.
     *       [Ref IERC721Metadata - tokenURI].
     *
-    * @param tokenId    Token to return its URI.
+    * @param tokenId Token to return its URI.
+    *
+    * @return string Token URI.
     */
     function tokenURI(uint256 tokenId) external view returns (string memory) {
-        /// @dev Does nothing for now.
         /**
         * @dev  Maybe the constructor might be modified to take in a base URI
         *       for the tokens when this is instantiated.
@@ -147,9 +148,9 @@ contract DaccredBase {
     }
 
     /**
-    * @dev  Returns the total supply of tokens in this network.
+    * @dev Returns the total supply of tokens in this network.
     *
-    * @return Token total supply.
+    * @return uint256 Token total supply.
     */
     function totalSupply() public view returns(uint256) {
         /// @dev Returns the value of the number of tokens minted - the number of tokens burned.
@@ -159,7 +160,7 @@ contract DaccredBase {
     /**
     * @dev Returns the absolute total count of minted tokens (including those burned).
     *
-    * @return uint256 absolute token total.
+    * @return uint256 Absolute token total.
     */
     function totalMints() public view returns(uint256) {
         /// @dev Returns the absolute token total.
@@ -175,9 +176,9 @@ contract DaccredBase {
         ///         all function selectors in the interface. See: https://eips.ethereum.org/EIPS/eip-165
         ///         e.g. `bytes4(i.functionA.selector ^ i.functionB.selector ^ ...)`
         return
-            interfaceId == 0x01ffc9a7 ||    /// @dev    ERC165 interface ID for ERC165.
-            interfaceId == 0x80ac58cd ||    /// @dev    ERC165 interface ID for ERC721.
-            interfaceId == 0x5b5e139f;      /// @dev    ERC165 interface ID for ERC721Metadata.
+            interfaceId == 0x01ffc9a7 ||    /// @dev ERC165 interface ID for ERC165.
+            interfaceId == 0x80ac58cd ||    /// @dev ERC165 interface ID for ERC721.
+            interfaceId == 0x5b5e139f;      /// @dev ERC165 interface ID for ERC721Metadata.
     }
 
     /**
@@ -193,33 +194,33 @@ contract DaccredBase {
     *       that is not a DEAD address is found. That address, owns every token infront of it, until
     *       another valid address mints, and it goes on.
     *
-    * @param _tokenId :: The token Id to return the owner.
+    * @param _tokenId The token Id to return the owner.
     */
     function ownerOf(uint256 _tokenId) public view returns(address ownerOfToken) {
-        /// @dev    Require that the tokenId exists.
+        /// @dev Require that the tokenId exists.
         require(exists(_tokenId), "Query for non-existent token");
-        /// @dev    This address holds the address we shall return.
+        /// @dev This address holds the address we shall return.
         address addressToFind;
-        /// @dev    Assign the _tokenId to an index.
+        /// @dev Assign the _tokenId to an index.
         uint256 tokenIndex = _tokenId;
 
         /// @dev    Starting from that index, it will iterate back until it lands on a token mapped to
         ///         an address that is not a Zero or DEAD address.
         for (uint i = tokenIndex; i >= 1; i--) {
-            /// @dev    If the token is mapped to an address that is not DEAD and not Zero, then break.
+            /// @dev If the token is mapped to an address that is not DEAD and not Zero, then break.
             if ((tokens[i] != DEAD) && (tokens[i] != address(0))) {
-                /// @dev    Assign that address to the address to be found.
+                /// @dev Assign that address to the address to be found.
                 addressToFind = tokens[i];
-                /// @dev    Break.
+                /// @dev Break.
                 break;
             }else {
-                /// @dev    Else:
-                /// @dev    Skip that address map.
+                /// @dev Else:
+                /// @dev Skip that address map.
                 continue;
             }
         }
 
-        /// @dev    Return the found address.
+        /// @dev Return the found address.
         ownerOfToken = addressToFind;
     }
 
@@ -229,21 +230,21 @@ contract DaccredBase {
     *       Verifying that the first address is the owner of the contract it mints `quantity`
     *       number of tokens to the owner of the contract.
     *       
-    * @notice   _address must be the owner.
+    * @notice _address must be the owner.
     *
     * @param _address   Required address of the owner of the contract instantiation.
     *                   This will be passed from the Factory e.g `mint(msg.sender, 6);`.
     * @param quantity   Number of tokens to be minted.
     */
     function mint(address _address, uint256 quantity) public {
-        /// @dev    Ensure caller is not 0 address.
+        /// @dev Ensure caller is not 0 address.
         require(msg.sender != address(0), "Mint by 0 Address");
-        /// @dev    Ensure address is the owner.
+        /// @dev Ensure address is the owner.
         require(_address == owner, "!Owner");
-        /// @dev    Ensure the quantity is GT 0.
+        /// @dev Ensure the quantity is GT 0.
         require(quantity != 0, "Minting 0 tokens");
-        /// @dev    From Solidity versions GT 0.8, the compiler automatically checks for overflows.
-        /// @dev    Mint `quantity` amount of tokens to caller.
+        /// @dev From Solidity versions GT 0.8, the compiler automatically checks for overflows.
+        /// @dev Mint `quantity` amount of tokens to caller.
         _mint(owner, quantity);
     }
 
@@ -253,7 +254,7 @@ contract DaccredBase {
     *       Verifying that the first address is the owner of the contract it mints `quantity`
     *       number of tokens to the address specified as the second argument of the contract function.
     *       
-    * @notice   _address must be the owner.
+    * @notice _address must be the owner.
     *
     * @param _address   Required address of the owner of the contract instantiation.
     *                   This will be passed from the Factory e.g `mint(msg.sender, 6);`.
@@ -265,17 +266,17 @@ contract DaccredBase {
         uint256 quantity
     ) public 
     {
-        /// @dev    Ensure caller is not 0 address.
+        /// @dev Ensure caller is not 0 address.
         require(msg.sender != address(0), "Sent from 0 Address");
-        /// @dev    Ensure address is the owner.
+        /// @dev Ensure address is the owner.
         require(_address == owner, "!Owner");
-        /// @dev    Ensure receiver is not 0 address.
+        /// @dev Ensure receiver is not 0 address.
         require(_to != address(0), "Mint to 0 Address");
-        /// @dev    Ensure the quantity is GT 0.
+        /// @dev Ensure the quantity is GT 0.
         require(quantity != 0, "Minting 0 tokens");
-        /// @dev    From Solidity versions GT 0.8, the compiler automatically checks for overflows.
-        /// @dev    Mint `quantity` amount of tokens to caller.
-        /// @dev    Should the owner pass his address as `_to`, it should mint to that address.
+        /// @dev From Solidity versions GT 0.8, the compiler automatically checks for overflows.
+        /// @dev Mint `quantity` amount of tokens to caller.
+        /// @dev Should the owner pass his address as `_to`, it should mint to that address.
         _mint(_to, quantity);
     }
 
@@ -289,11 +290,11 @@ contract DaccredBase {
     * @return bool.
     */
     function isApproved(uint256 tokenId, address _address) public view returns(bool) {
-        /// @dev    Makes sure that the tokenId is existent.
+        /// @dev Makes sure that the tokenId is existent.
         require(exists(tokenId), "Query for non-existent token.");
-        /// @dev    Returns true if the _address is approved by the token owner.
+        /// @dev Returns true if the _address is approved by the token owner.
         bool isApprovedByTokenOwner = (tokenApprovals[tokenId] == _address);
-        /// @dev    Evaluate and return.
+        /// @dev Evaluate and return.
         return isApprovedByTokenOwner;
     }
 
@@ -301,42 +302,42 @@ contract DaccredBase {
     * @dev  Returns true if the `_address` is approved by the owner of the contract to transfer
     *       all tokens owned by owner.
     * 
-    * @param _address   Address to check if it is approved to spend or transfer all tokens.
+    * @param _address Address to check if it is approved to spend or transfer all tokens.
     *
     * @return bool.
     */
     function approvedForAll(address _address) public view returns(bool) {
-        /// @dev    Returns true if the owner of the token has approved him to send all.
+        /// @dev Returns true if the owner of the token has approved him to send all.
         bool isApprovedByOperator = (_operatorApprovals[owner][_address]);
-        /// @dev    Evaluate and return.
+        /// @dev Evaluate and return.
         return isApprovedByOperator;
     }
 
     /**
-    * @dev  Returns True if the token in question is owned by the owner of the contract.
+    * @dev Returns True if the token in question is owned by the owner of the contract.
     *
-    * @param tokenId    Token to evaluate.
+    * @param tokenId Token to evaluate.
     *
     * @return bool.
     */
     function isOwnedByOwner(uint256 tokenId) public view returns(bool) {
-        /// @dev    Evaluate that the owner of the tokenId is the owner of the contract.
+        /// @dev Evaluate that the owner of the tokenId is the owner of the contract.
         return (ownerOf(tokenId) == owner);
     }
 
     /**
-    * @dev  Returns true if the `tokenId` passed exists.
+    * @dev Returns true if the `tokenId` passed exists.
     *
-    * @param tokenId    Token to valdate.
+    * @param tokenId Token to valdate.
     *
     * @return bool.
     */
     function exists(uint256 tokenId) private view returns(bool) {
-        /// @dev    Require that the tokenId passed is less than the number of tokens minted.
+        /// @dev Require that the tokenId passed is less than the number of tokens minted.
         bool isValidTokenNumber = (tokenId > 0) && (tokenId <= tokensMinted);
-        /// @dev    Require that the address owning the tokenId is not a DEAD address, meaning that the token was burnt and doesn't exist.
+        /// @dev Require that the address owning the tokenId is not a DEAD address, meaning that the token was burnt and doesn't exist.
         bool isAlive = tokens[tokenId] != DEAD;
-        /// @dev    All checks passed.
+        /// @dev All checks passed.
         return isValidTokenNumber && isAlive;
     }
 
@@ -349,15 +350,15 @@ contract DaccredBase {
     * @param _quantity  Quantity to be minted.
     */
     function _mint(address _to, uint256 _quantity) private {
-        /// @dev    Get the current next number to be minted.
+        /// @dev Get the current next number to be minted.
         uint256 currentMint = totalMints() + 1;
-        /// @dev    Assign it to the `_to` address.
+        /// @dev Assign it to the `_to` address.
         tokens[currentMint] = _to;
-        /// @dev    Increment the tokensMinted and add the `_quantity` minted.
+        /// @dev Increment the tokensMinted and add the `_quantity` minted.
         tokensMinted += _quantity;
-        /// @dev    Increment the callers totalBalance of tokens.
+        /// @dev Increment the callers totalBalance of tokens.
         tokenBalances[msg.sender] += _quantity;
-        /// @dev    Emit the {Mint} event.
+        /// @dev Emit the {Mint} event.
         emit BaseMint(address(0), _to);
     }
 }
