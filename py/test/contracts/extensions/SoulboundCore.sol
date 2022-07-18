9f713203d64b7379dfd75117264a6e7544e17644
fps <anthony.nnaemeka.umeh@gmail.com>
2022-07-18 08:23:42 +0100

    Add files via upload

---
+++ py/test/contracts/extensions/SoulboundCore.sol
@@ -0,0 +1,198 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "./Soulbound.sol";
+import "./Ownable.sol";
+import "./Allowlist.sol";
+
+/**
+* @title Soulbound Core Contract.
+* @author Daccred.
+* @dev  Soulbound Core template. This contract aims at a soulbound token with
+*       capped supply, set by the deployer or defaulted to 1000000.
+*       Mints and burns affect the current supply of tokens respectively.
+*/
+contract SoulboundCore is Ownable, Soulbound, Allowlist {
+    /// @dev Total supply limit set by deployer or defaulted to 1000000.
+    uint256 internal totalSupply;
+    /// @dev    With every issue and revoke, this value
+    ///         increases and reduces.
+    ///         It cannot be GT the TOTAL_SUPPLY.
+    uint256 internal supply;
+
+    /// @dev Emitted when a token is minted from Signature.
+    event IssueWithSignature(address indexed to, uint256 indexed tokenId);
+    /// @dev Emitted when a token is revoked with Signature.
+    event RevokeWithSignature(uint256 indexed tokenId);
+
+    /**
+    * @dev  Security check to require that the address calling a particular
+    *       function is the allowlistOwner.
+    *       
+    * @param _caller Address.
+    */
+    modifier onlyAllowlistOwner(address _caller) {
+        require(_caller == getAllowlistOwner(), "Not Allowlist Owner!");
+        _;
+    }
+
+    /// @dev Deploys the 3 contracts inherited by the SoulboundCore.
+    constructor(
+        string memory name, 
+        string memory symbol,
+        address _allowlistOwner,
+        uint256 _totalSupply
+    )
+    Soulbound(name, symbol)
+    Allowlist(_allowlistOwner) {
+        if (_totalSupply == 0) {
+            totalSupply = 1e6;
+        } else {
+            totalSupply = _totalSupply;
+        }
+    }
+
+    /**
+    * @dev  Mints a particular quantity of tokens to `to`, 
+    *       on the condition that the address has been 
+    *       signed by the allowlistOwner off-chain.
+    *       This will emit the {MintSoulboundToken} event
+    *       from the Soulbound.sol.
+    *
+    * @notice Callable by anyone.
+    *
+    * @param addr       Address to mint tokens to.
+    * @param hash       Hashed message by the allowlistOwner.
+    * @param sig        Signature, signed by the allowlistOwner.
+    * @param tokenId    Id of the tokens to mint to the `addr`.
+    * @param tokenURI   URI of the token to be minted.
+    */
+    function issueWithSignature(
+        address addr,
+        bytes32 hash,
+        bytes memory sig,
+        uint256 tokenId,
+        string memory tokenURI
+    ) public
+    {
+        /// @dev Require that the address is not a zero address.
+        require(addr != address(0), "Mint to zero address.");
+        /// @dev    Require that the hash is actually 32 [64 characters]
+        ///         in length.
+        require(hash.length == 32, "Invalid hash.");
+        /// @dev Require the length of the signature is 65.
+        require(sig.length == 65, "Invalid signature length");
+        /// @dev    Verifies that the address was actually signed by the
+        ///         allowlistOwner.
+        require(verifySignature(hash, sig), "Hash not signed by owner.");
+        /// @dev    Mint the tokens to address.
+        ///         [Ref Soulbound.sol].
+        issue(addr, tokenId, tokenURI);
+        /// @dev Emit the IssueWithSignature event.
+        emit IssueWithSignature(addr, tokenId);
+    }
+
+    /**
+    * @dev  Revokes the ownership of `tokenId` from the owner.
+    *       The token must exist and the signature must be signed the
+    *       allowlistOwner.
+    *       This emits the {RevokeWithSignature} event.
+    *
+    * @notice Callable by anyone.
+    *
+    * @param hash       Hashed message by the allowlistOwner.
+    * @param sig        Signature, signed by the allowlistOwner.
+    * @param tokenId    Id of the token to revoke.
+    */
+    function revokeWithSignature(
+        bytes32 hash,
+        bytes memory sig,
+        uint256 tokenId
+    ) public
+    {
+        /// @dev Require that the token exists.
+        require(_exists(tokenId), "Revoke of inexistent token.");
+        /// @dev    Require that the hash is actually 32 [64 characters]
+        ///         in length.
+        require(hash.length == 32, "Invalid hash.");
+        /// @dev Require the length of the signature is 65.
+        require(sig.length == 65, "Invalid signature length");
+        /// @dev    Verifies that the address was actually signed by the
+        ///         allowlistOwner.
+        require(verifySignature(hash, sig), "Hash not signed by owner.");
+        /// @dev    Mint the tokens to address.
+        ///         [Ref Soulbound.sol].
+        revoke(ownerOf(tokenId), tokenId);
+        /// @dev Emit the RevokeWithSignature event.
+        emit RevokeWithSignature(tokenId);
+    }
+
+    /**
+    * @dev  Allows the `caller` (allowlistOwner) to set the baseURI.
+    *       This is really important when the caller wants to mint
+    *       Multiple tokens with the same base URI.
+    *
+    * @notice   Callable by the deployer of this contract [DaccredDeployer]
+    *           and the allowlistOwner.
+    */
+    function setBaseURI(address caller, string memory _baseURI) 
+    public 
+    onlyOwner 
+    onlyAllowlistOwner(caller)
+    {
+        /// @dev Set baseURI.
+        _setBaseURI(_baseURI);
+    }
+
+    /**
+    * @dev  Using the `tokenId` passed, it generates a `stringified` tokenURI,
+    *       packing the baseURI and the current tokenId.
+    *       Makes use of OpenZeppelin's uint to string function.
+    *
+    * @notice Callable by anyone.
+    *
+    * @param tokenId ID of token whose tokenURI is desired.
+    *
+    * @return _tokenURI TokenURI of the passed tokenId.
+    */
+    function generateTokenURI(uint256 tokenId) 
+    public 
+    view
+    returns(string memory _tokenURI)
+    {
+        /// @dev Require baseURI length is not currently 0.
+        require(bytes(_getBaseURI()).length != 0, "Empty baseURI");
+        /// @dev Return a packed tokenURI string.
+        _tokenURI = string(abi.encodePacked(
+            _getBaseURI(),
+            toString(tokenId)
+        ));
+    }
+
+    /**
+    * @dev Converts a `uint256` to its ASCII `string` decimal representation.
+    *      Copied from OpenZeppelin.
+    */
+    function toString(uint256 value) internal pure returns (string memory) {
+        // Inspired by OraclizeAPI's implementation - MIT licence
+        // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
+
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
+    }
+
+}
---
+++ py/test/contracts/extensions/SoulboundWithSignature.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "./SoulboundCore.sol";
+
+/**
+* @title SoulboundWithSignature.
+* @author Daccred.
+* @dev  SoulboundWithSignature contract template allows freedom of
+*       issuing and revoking tokens with Signature while controlling
+*       the totalSupply of the token.
+*/
+contract SoulboundWithSignature is SoulboundCore {
+    /// @dev Deploys inherited contract and sub contracts.
+    constructor(
+        string memory name, 
+        string memory symbol,
+        address _allowlistOwner,
+        uint256 _totalSupply
+    )
+    SoulboundCore(
+        name, 
+        symbol, 
+        _allowlistOwner, 
+        _totalSupply
+    ) {}
+
+    /**
+    * @dev Ref SoulboundCore.sol issueWithSignature
+    *       This function grants the access to only the
+    *       deployer of the contract, unlike the core
+    *       that allows the function for anyone who has a
+    *       signature signed by the allowlistOwner.
+    *       This contract can be called by the deployer of the
+    *       contract [DaccredDeployer] but is also protected
+    *       as to the allowlistOwner must be the signer of the `sig.`
+    *
+    * @notice Callable by the deployer of this contract [DaccredDeployer].
+    *
+    * @param addr       Address to be minted to.
+    * @param hash       Hash of message signed.
+    * @param sig        Signature.
+    * @param tokenId    TokenId to be issued.
+    * @param tokenURI   URI of token to be issued.
+    */
+    function ownerIssueWithSignature(
+        address addr,
+        bytes32 hash,
+        bytes memory sig,
+        uint256 tokenId,
+        string memory tokenURI
+    ) public onlyOwner
+    {
+        /// @dev Ensure that the supply is not crossed.
+        /// @dev    Should all soulbound tokens need to be limited,
+        ///         copy this code and paste in Soulboundcore.sol
+        ///         issueWithSignature function.
+        require(supply < totalSupply, "Issue Cap Reached.");
+        /// @dev Issue With Signature.
+        issueWithSignature(
+            addr,
+            hash,
+            sig,
+            tokenId,
+            tokenURI
+        );
+        /// @dev Incrememt supply on successful issue.
+        supply++;
+    }
+
+    /**
+    * @dev Ref SoulboundCore.sol revokeWithSignature
+    *       This function grants the access to only the
+    *       deployer of the contract, unlike the core
+    *       that allows the function for anyone who has a
+    *       signature signed by the allowlistOwner.
+    *       This contract can be called by the deployer of the
+    *       contract [DaccredDeployer] but is also protected
+    *       as to the allowlistOwner must be the signer of the `sig.`
+    *
+    * @notice Callable by the deployer of this contract [DaccredDeployer].
+    *
+    * @param hash       Hash of message signed.
+    * @param sig        Signature.
+    * @param tokenId    TokenId to be issued.
+    */
+    function ownerRevokeWithSignature(
+        bytes32 hash,
+        bytes memory sig,
+        uint256 tokenId
+    ) public onlyOwner
+    {
+        /// @dev    If the supply control is not 0,
+        ///         decrement the supply.
+        ///         Should all soulbound tokens need to be limited,
+        ///         copy this code and paste in Soulboundcore.sol
+        ///         revokeWithSignature function.
+        if (supply != 0) {
+            /// @dev Decrement supply.
+            supply--;
+        } else {
+            /// @dev Throw error if 0 is reached.
+            revert("Lowest limit reached.");
+        }
+
+        /// @dev Revoke With Signature.
+        revokeWithSignature(
+            hash,
+            sig,
+            tokenId
+        );
+    }
+}
