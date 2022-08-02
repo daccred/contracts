38f0aec629dc3a2e0dd3e0f677bc49bd1799f97f
toby0318 <78232000+toby0318@users.noreply.github.com>
2022-08-03 01:58:54 +0900

    Updated test file
    Updated extension and test file

--- packages/nft/test/extensions/ERC721ExtensionSignature.test.ts
+++ packages/nft/test/extensions/ERC721ExtensionSignature.test.ts
@@ -22,41 +22,58 @@ const createTestSuite = ({ contract, constructorArgs }: any) =>
         this.burnedTokenId = 1;
 
         this.hash = await ethers.utils.hashMessage("Minting");
-        this.signature = await addr1.signMessage(hash);
-        await this.erc721ExtensionWithSignature.connect(this.addr1).mint(this.addr1.address, "test1");
+        this.signature = await this.owner.signMessage(this.hash);
+        await this.erc721ExtensionWithSignature.connect(this.addr1).mint("test1");
       });
 
       it('mint with signature', async function () {
-        const query = this.erc721ExtensionWithSignature.connect(this.addr2).mintWithSignature(this.addr1.address, this.hash, this.signature, "test2");
-        await expect(query).to.equal(2);
+        await this.erc721ExtensionWithSignature.connect(this.owner).mintWithSignature(this.addr1.address, this.hash, this.signature, "test2");
+        expect(await this.erc721ExtensionWithSignature.balanceOf(this.addr1.address)).to.equal(2);
+      });
+
+      it('cannot burn with invalid owner', async function () {
+        const query = this.erc721ExtensionWithSignature.connect(this.addr2).burn(1);
+        await expect(query).to.be.revertedWith('Caller is not owner of the token.');
+      });
+
+      it('burn with valid owner', async function () {
+        await this.erc721ExtensionWithSignature.connect(this.addr1).burn(1);
+        expect(await this.erc721ExtensionWithSignature.balanceOf(this.addr1.address)).to.equal(0);
+      });
+
+      it('cannot mint over capped supply', async function () {
+        await this.erc721ExtensionWithSignature.connect(this.addr1).mint("test1");
+        const query = this.erc721ExtensionWithSignature.connect(this.addr1).mint("test2");
+        await expect(query).to.be.revertedWith("You can't mint anymore.");
       });
 
       it('cannot mint to zero address', async function () {
         const query = this.erc721ExtensionWithSignature
-          .connect(this.addr2)
-          .mintWithSignature("0x0000000000000000000000000000000000000000", this.hash, this.signature, "test3");
+          .connect(this.owner)
+          .mintWithSignature(ZERO_ADDRESS, this.hash, this.signature, "test3");
         await expect(query).to.be.revertedWith('Mint to zero address.');
       });
 
       it('cannot mint with invalid hash', async function () {
+        const hash = await ethers.utils.hashMessage("Minting1");
         const query = this.erc721ExtensionWithSignature
-          .connect(this.addr2)
-          .mintWithSignature(this.addr1.address, "xxx", this.signature, "test3");
+          .connect(this.owner)
+          .mintWithSignature(this.addr1.address, hash, this.signature, "test3");
         await expect(query).to.be.revertedWith('Invalid hash.');
       });
 
-      it('cannot mint with invalid signature length', async function () {
+      it('cannot mint with Hash not signed by owner.', async function () {
+        const sig = await this.addr1.signMessage(this.hash);
         const query = this.erc721ExtensionWithSignature
-          .connect(this.addr2)
-          .mintWithSignature(this.addr1.address, this.hash, "ab", "test3");
-        await expect(query).to.be.revertedWith('Invalid signature length');
+          .connect(this.owner)
+          .mintWithSignature(this.addr2.address, this.hash, sig, "test3");
+        await expect(query).to.be.revertedWith('Hash not signed by owner.');
       });
 
-      it('cannot mint with Hash not signed by owner.', async function () {
-        const query = this.erc721ExtensionWithSignature
-          .connect(this.addr2)
-          .mintWithSignature(this.addr2.address, this.hash, this.signature, "test3");
-        await expect(query).to.be.revertedWith('Hash not signed by owner.');
+      context('owner()', function () {
+        it('has the expected value', async function () {
+          expect(await this.erc721ExtensionWithSignature.owner()).to.equal(this.owner.address);
+        });
       });
 
       context('currentSupply()', function () {
@@ -67,11 +84,11 @@ const createTestSuite = ({ contract, constructorArgs }: any) =>
 
       context('balanceOf()', function () {
         it('has the expected value', async function () {
-          expect(await this.erc721ExtensionWithSignature.balanceOf(this.addr1.address)).to.equal(2);
+          expect(await this.erc721ExtensionWithSignature.balanceOf(this.addr1.address)).to.equal(this.owner.address);
         });
       });
     });
   };
 
-describe('erc721ExtensionWithSignature', createTestSuite({ contract: 'erc721ExtensionWithSignature', constructorArgs: ['Daccred', 'DCD'] }));
+describe('ERC721ExtensionSignature', createTestSuite({ contract: 'ERC721ExtensionSignature', constructorArgs: ['Daccred', 'DCD', 2 ] }));
 
